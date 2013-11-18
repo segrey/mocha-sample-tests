92ac8cc267c1e4c316e550ca7c6f04af08f4e8d8
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-11-18 18:42:27 +0400

    test with 'hasOwnProperty' from Object.prototype

--- pure-jasmine/jasmine-test.js
+++ pure-jasmine/jasmine-test.js
@@ -1,3 +1,9 @@
+describe("hasOwnProperty", function() {
+  it("test", function () {
+    expect(true).to.be(true);
+  });
+});
+
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);

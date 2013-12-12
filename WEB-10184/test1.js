457358e404e296e6eb1e9031eaeab65f2443ce6f
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-12-12 19:47:26 +0400

    remove webserver launching

--- WEB-10184/test1.js
+++ WEB-10184/test1.js
@@ -1,13 +1,5 @@
 var expect = require('chai').expect;
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
-
-
 describe('Suite1', function () {
   describe('inner suite', function () {
     it('should pass', function () {

9c811f92cac156484e472388e326f3d520a959da
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2017-05-31 11:53:14 +0300

    update chai test

--- chai-example/chai-test.js
+++ chai-example/chai-test.js
@@ -4,14 +4,10 @@ var assert = chai.assert;
 var should = chai.should(); //actually call the function
 
 describe('chai', function () {
-  it('should', function () {
+  it('should check indexOf', function () {
     [1, 2, 3].indexOf(4).should.equal(-1);
   });
-  it('expect', function () {
+  it('should pass', function () {
     expect(true).to.be.equal(true);
-  });
-  it('assert', function () {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    expect('First line\nSecond line wrong\nThird line').to.equal('First line\nSecond line\nThird line');
   });
 });

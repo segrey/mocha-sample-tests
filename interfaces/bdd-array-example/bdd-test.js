3bbe7ab0cb514960bafb9fa7e0a2f709f143ff56
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2017-05-31 13:45:33 +0300

    use should

--- interfaces/bdd-array-example/bdd-test.js
+++ interfaces/bdd-array-example/bdd-test.js
@@ -1,13 +1,31 @@
-describe('async Array ', function () {
-  describe('#indexOf()', function () {
-    it("fail", function (done) {
-      setTimeout(function() {
-        [1,2,3].indexOf(5).should.equal(1);
-        done();
-      }, 0);
+require('chai').should();
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('fail', function (done) {
+            setTimeout(function() {
+                [1,2,3].indexOf(5).should.equal(1);
+                done();
+            }, 0);
+        });
+        it('pass', function () {
+            [1,2,3].indexOf(5).should.equal(-1);
+        });
     });
-    it('pass', function () {
-      [1,2,3].indexOf(5).should.equal(-1);
-    });
+
+  it('compare files', function () {
+    var actualFilePath = require.resolve('chai'), expectedFilePath = __filename;
+    try {
+      lastModifiedTime(actualFilePath).should.equal(lastModifiedTime(expectedFilePath));
+    }
+    catch (err) {
+      err.actualFilePath = actualFilePath;
+      err.expectedFilePath = expectedFilePath;
+      throw err;
+    }
   });
 });
+
+function lastModifiedTime(path) {
+  return require('fs').statSync(path).mtime.getTime();
+}
